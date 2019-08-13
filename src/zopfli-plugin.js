82ac503388be356342aa5f482e2e495e11aba344
Andrew Grishin <AG-Teammate@users.noreply.github.com>
2019-08-13 16:12:09 -0400

    added option to compress files in parallel

--- src/zopfli-plugin.js
+++ src/zopfli-plugin.js
@@ -7,22 +7,35 @@ const compressFile = require('./compressFile')
 
 const defaultOptions = {
   extensions: ['css', 'js'],
-  path: ''
+  path: '',
+  parallel: false
 }
 
 const globAsync = util.promisify(glob)
 
-async function onPostBuild (args, pluginOptions) {
-  const options = { ...defaultOptions, ...pluginOptions }
+async function onPostBuild(args, pluginOptions) {
+  const options = {...defaultOptions, ...pluginOptions}
   const fileBasePath = path.join(process.cwd(), 'public')
   const patternExt = (options.extensions.length > 1) ? `{${options.extensions.join(',')}}` : options.extensions[0]
   const pattern = `**/*.${patternExt}`
 
-  const files = await globAsync(pattern, { cwd: fileBasePath, ignore: '**/*.gz', nodir: true })
-  const compress = files.map(file => {
-    return compressFile(file, pluginOptions)
-  })
-  return Promise.all(compress)
+  const files = await globAsync(pattern, {cwd: fileBasePath, ignore: '**/*.gz', nodir: true})
+  if (!options.parallel) {
+    const compress = files.map(file => {
+      return compressFile(file, pluginOptions)
+    })
+    return Promise.all(compress)
+  } else {
+    const workerFarm = require('worker-farm')
+    const workers = workerFarm(require.resolve('./worker'))
+    const compress = files.map(file => {
+      return new Promise((resolve, reject) => {
+        workers(file, pluginOptions, err => err ? reject(err) : resolve())
+      })
+    })
+    await Promise.all(compress)
+    workerFarm.end(workers)
+  }
 }
 
 exports.onPostBuild = onPostBuild
