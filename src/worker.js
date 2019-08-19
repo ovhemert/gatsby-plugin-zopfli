174b1ab47647b6091b340771726200ead501b221
Osmond van Hemert <osmond@vanhemert.nu>
2019-08-19 22:43:20 +0200

    make parallel processing standard

--- src/worker.js
+++ src/worker.js
@@ -1,4 +1,28 @@
-const compressFile = require('./compressFile')
+'use strict'
+
+const zopfli = require('@gfx/zopfli')
+const path = require('path')
+const util = require('util')
+const fs = require('fs')
+const mkdirp = require('mkdirp')
+const mkdirpAsync = util.promisify(mkdirp)
+const readFileAsync = util.promisify(fs.readFile)
+const writeFileAsync = util.promisify(fs.writeFile)
+
+async function compressFile (file, pluginOptions = {}) {
+  // zopfli-gzip the asset to a new file with the .gz extension
+  const fileBasePath = path.join(process.cwd(), 'public')
+  const srcFileName = path.join(fileBasePath, file)
+  const content = await readFileAsync(srcFileName)
+  const compressed = await zopfli.gzipAsync(content, {})
+
+  const destFilePath = (pluginOptions.path) ? path.join(fileBasePath, pluginOptions.path) : fileBasePath
+  const destFileName = path.join(destFilePath, file) + '.gz'
+  const destFileDirname = path.dirname(destFileName)
+
+  await mkdirpAsync(destFileDirname)
+  await writeFileAsync(destFileName, compressed)
+}
 
 module.exports = function (file, options, callback) {
   compressFile(file, options)
--- src/zopfli-plugin.js
+++ src/zopfli-plugin.js
@@ -3,12 +3,12 @@
 const glob = require('glob')
 const path = require('path')
 const util = require('util')
-const compressFile = require('./compressFile')
+const workerFarm = require('worker-farm')
+const worker = require.resolve('./worker')
 
 const defaultOptions = {
   extensions: ['css', 'js'],
-  path: '',
-  parallel: false
+  path: ''
 }
 
 const globAsync = util.promisify(glob)
@@ -20,22 +20,19 @@ async function onPostBuild (args, pluginOptions) {
   const pattern = `**/*.${patternExt}`
 
   const files = await globAsync(pattern, { cwd: fileBasePath, ignore: '**/*.gz', nodir: true })
-  if (!options.parallel) {
-    const compress = files.map(file => {
-      return compressFile(file, pluginOptions)
+  const tmrStart = new Date().getTime()
+
+  const compressFile = workerFarm(worker)
+  const compress = files.map(file => {
+    return new Promise((resolve, reject) => {
+      compressFile(file, pluginOptions, err => err ? reject(err) : resolve())
     })
-    return Promise.all(compress)
-  } else {
-    const workerFarm = require('worker-farm')
-    const workers = workerFarm(require.resolve('./worker'))
-    const compress = files.map(file => {
-      return new Promise((resolve, reject) => {
-        workers(file, pluginOptions, err => err ? reject(err) : resolve())
-      })
-    })
-    await Promise.all(compress)
-    workerFarm.end(workers)
-  }
+  })
+  await Promise.all(compress)
+  workerFarm.end(compressFile)
+
+  const tmrEnd = new Date().getTime()
+  console.log(`Zopfli compressed ${files.length} files in ${(tmrEnd - tmrStart) / 1000} s`)
 }
 
 exports.onPostBuild = onPostBuild
